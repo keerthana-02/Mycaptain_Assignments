25e404209827eb14d10a2c695b4a8d88b430ab0b
keerthana-02 <90972784+keerthana-02@users.noreply.github.com>
2021-12-24 21:36:34 +0530

    Add files via upload

---
+++ positive number.py
@@ -0,0 +1,13 @@
+#displaying positive numbers in range(1):
+
+list1 = [12,-7,5,64,-14]
+for i in list1:
+    if i>= 0:
+       print(i,end=" ")
+       
+#displaying positive numbers in range(2):
+
+list2 = [12,14,-95,3]
+for i in list2:
+    if i >= 0:
+        print(i,end=" ")
